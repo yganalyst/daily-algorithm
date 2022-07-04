7bb551f9e1083d95590c6c6da3981c9532b6501a
yganalyst <yg_analyst@naver.com>
2022-07-05 00:00:47 +0900

    20200704

--- baekjoon/1065_한수.py
+++ baekjoon/1065_한수.py
@@ -6,6 +6,8 @@
 - Problem : 1~N개의 정수 중 한수(각 자리가 등차수열) 구하기
 
 *나올수있는 조건을 좀 더 생각하고 구현하기
+*1~99는 등차수열로 봄
+*str도 iterable 객체이므로 map에 바로 들어갈 수 있음
 
 ex)
 input:
@@ -15,17 +17,17 @@ output:
 
 """
 
-N=110
+N=int(input())
 cnt=0
 for i in range(1,N+1):
-    i=11
-    num = list(map(int,list(str(i))))
-    for j in range(len(num)):
-        j=0
-        
-        num[0]
-
-
-    len(i)    
-    99-10+1+9
-10-1
+    if i > 99:
+        i=100
+        num = list(map(int,str(i)))
+        st_ = set()
+        for j in range(len(num)-1):
+            st_.add(num[j+1]-num[j])
+        if len(st_)==1:
+            cnt+=1
+    else:
+        cnt+=1
+print(cnt)
