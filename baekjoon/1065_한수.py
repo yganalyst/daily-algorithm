14ec4978f659023ac309521964357dc645af6b09
yganalyst <yg_analyst@naver.com>
2022-07-05 00:12:01 +0900

    20200704

--- baekjoon/1065_한수.py
+++ baekjoon/1065_한수.py
@@ -8,6 +8,7 @@
 *나올수있는 조건을 좀 더 생각하고 구현하기
 *1~99는 등차수열로 봄
 *str도 iterable 객체이므로 map에 바로 들어갈 수 있음
+*나머지를 이용한 방법 참고
 
 ex)
 input:
@@ -17,6 +18,7 @@ output:
 
 """
 
+# solve
 N=int(input())
 cnt=0
 for i in range(1,N+1):
@@ -31,3 +33,21 @@ for i in range(1,N+1):
     else:
         cnt+=1
 print(cnt)
+
+# reference
+def Hansu(N):
+    cnt=0
+    if N<100:
+        return N
+    else:
+        for i in range(100,N+1):
+            n100 = i//100
+            n10 = (i%100)//10
+            n1 = i%10
+            if (n100-n10)==(n10-n1):
+                cnt+=1
+        return 99+cnt
+Hansu(int(input()))
+
+
+
