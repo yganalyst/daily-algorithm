7c7f77b42ad66627388df06f9fbc544d355f03cd
yganalyst <yg_analyst@naver.com>
2022-07-10 11:47:48 +0900

    20220710

--- book/2_구현_게임개발.py
+++ book/2_구현_게임개발.py
@@ -9,25 +9,52 @@
         (단, 뒤가 바다인 경우 움직임을 멈춤)
         0:북, 1:동, 2:남, 3:서
 """
-grid_size = 4,4
-start, head = (1,1),0
-grid_map = [[1,1,1,1],
-            [1,0,0,1],
-            [1,1,0,1],
-            [1,1,1,1]]
+grid_size = list(map(int,input().split()))
+x,y, head = map(int,input().split())
+grid_map=[]
+for i in range(grid_size[0]):
+    grid_map.append(list(map(int,input().split())))
+
+start = [x,y]    
 step_ = [0,3,2,1]
+hist_ = []
+hist_.append(start)
 
 def is_valid(point):
-    if (start[0]>=0) & (start[1]>=0) & \
-        (start[0]<=grid_size[1]) & (start[1]<=grid_size[1]):
-        return True        
-    if grid_map[start[0]][start[1]]==0:
-        return True
+    cond1,cond2,cond3 = False,False,False
+    if (point[0]>=0) & (point[1]>=0) & \
+        (point[0]<=grid_size[1]) & (point[1]<=grid_size[1]):
+        cond1=True
+    if grid_map[point[0]][point[1]]==0:
+        cond2=True
+    if point not in hist_:
+        cond3=True
+    return all([cond1,cond2,cond3])
+
+def routing(head,point):
+    point_new = point[:]
+    if head==0:
+        point_new[0]-=1
+    elif head==1:
+        point_new[1]+=1
+    elif head==2:
+        point_new[0]+=1
+    elif head==3:
+        point_new[1]-=1
+    return point_new
 
 while True:
     now_step_idx = step_.index(head)
+    bl_=False
     for i in range(1,5):
-        i=1
         next_step_idx = (now_step_idx+i)%4
-        print(step_[next_step_idx])
         head = step_[next_step_idx]
+        next_point = routing(head, start)
+        if is_valid(next_point):
+            start = next_point[:]
+            hist_.append(start)
+            bl_=True
+            break
+    if not bl_:
+        break
+print(len(hist_))
