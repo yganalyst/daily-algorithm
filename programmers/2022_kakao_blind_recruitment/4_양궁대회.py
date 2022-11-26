920d61b61dfef00d3ab41f832840e38a5f9eb7b4
yganalyst <yg_analyst@naver.com>
2022-11-26 22:58:15 +0900

    20221126

--- programmers/2022_kakao_blind_recruitment/4_양궁대회.py
+++ programmers/2022_kakao_blind_recruitment/4_양궁대회.py
@@ -9,11 +9,19 @@
 
 주의할점
     reference 참고 ㅠㅠ    
-    과녁판 점수뽑는 dfs함수, 어피치와 점수 비교하는 함수 2개로 구성
+    크게 3개의 함수로 구성
+    1. 과녁판 점수뽑는 dfs함수
+      - 각 과녁마다 {0개 맞추기, apeach보다 1개 더맞추기} 중 1개 선택하는 경우
+      - 맞추고 다음 과녁으로 넘어감 DFS 방식
+    
+    
+    2. 어피치와 점수 비교하는 함수 2개로 구성
+    
     
 
 *Referece
     https://yensr.tistory.com/24
+    https://kjhoon0330.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%96%91%EA%B6%81%EB%8C%80%ED%9A%8C-Python
 
 """
 
@@ -21,51 +29,57 @@ n=5
 info=[2,1,1,1,0,0,0,0,0,0,0]
 
 # Reference
-maxScore = 0		# 가장 큰 점수 차이
-maxList = []		# 가장 큰 점수 차이를 낸 배열 
-def ryanScore(index, score, n, apeach) :	# dfs 함수
-    if n == 0 : 				# 횟수가 0 이면 calScore() 호출
-        calScore(score, apeach)
-        return
-    
-    if index == 11: return		# 0~10까지의 점수만 존재
-    
-    sc = apeach[index]			# 어피치가 (index)점을 맞힌 횟수
-    for i in range(sc+2):		# 0부터 (index)+1까지 맞히는 경우만 고려
-        if n >= i:			# n보다 크면 안되니까 고려
-            score[index] = i
-            ryanScore(index+1, score, n-i, apeach)
-            score[index] = 0        
-    
-def calScore(ryan, apeach):		# 점수 계산 함수
-    global maxScore, maxList
-    rScore = 0	# 라이언 점수
-    aScore = 0	# 어피치 점수
-    
+import copy
+MAX_SCORE_DIFF = 0
+answers = []
+ 
+# 라이언과 어피치의 점수 차이를 계산하는 함수
+def calcScoreDiff(info, myshots):
+    enemyScore, myScore = 0, 0
     for i in range(11):
-        if ryan[i] == 0 and apeach[i] == 0: continue	# 둘다 0이면 패스
-            
-        if ryan[i] > apeach[i] : rScore += (10-i)	# 라이언이 더 많이 맞췄으면 점수획득
-        else : aScore += (10-i)				# 아니면 어피치 점수획득
-            
-    if rScore > aScore :		# 라이언 점수가 더 높을 때만 고려
-        diff = rScore - aScore
-        if diff > maxScore:		# 최대값 갱신
-            maxScore = diff
-            maxList = list(ryan)
-            
-        elif diff == maxScore:			# 최대값이 같을 경우
-            for i in range(11):	
-                if ryan[-i] > maxList[-i]:	# 낮은 점수를 많이 맞은 경우가 선택
-                    maxList = list(ryan)
-                    break
-                elif ryan[-i] < maxList[-i]:
-                    break
-                    
-                    
+        if (info[i], myshots[i]) == (0, 0):
+            continue
+        if info[i] >= myshots[i]:
+            enemyScore += (10 - i)
+        else:
+            myScore += (10 - i)
+    return myScore - enemyScore
+ 
+def dfs(info, myshots, n, i):
+    global MAX_SCORE_DIFF, answers
+    if i == 11:
+        if n != 0:
+            myshots[10] = n
+        scoreDiff = calcScoreDiff(info, myshots)
+        if scoreDiff <= 0:
+            return
+        result = copy.deepcopy(myshots)
+        if scoreDiff > MAX_SCORE_DIFF:
+            MAX_SCORE_DIFF = scoreDiff
+            answers = [result]
+            return
+        if scoreDiff == MAX_SCORE_DIFF:
+            answers.append(result)
+        return
+ 
+    # 점수 먹는 경우
+    print(myshots)
+    if info[i] < n:
+        myshots.append(info[i] + 1)
+        dfs(info, myshots, n - info[i] - 1, i + 1)
+        myshots.pop()
+ 
+    # 점수 안먹는 경우
+    myshots.append(0)
+    dfs(info, myshots, n, i + 1)
+    myshots.pop()
+ 
 def solution(n, info):
-    temp = [0 for i in range(11)]
-    ryanScore(0, temp, n, info)		# dfs
-    
-    if len(maxList)==0 : return [-1]
-    else: return maxList
+    global MAX_SCORE_DIFF, answers
+    dfs(info, [], n, 0)
+    if answers == []:
+        return [-1]
+    answers.sort(key = lambda x : x[::-1], reverse=True)
+    return answers[0]
+
+len(answers)
