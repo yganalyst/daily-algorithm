e6034bcf8e0ebe2f305ad84dd4bb458634e70a13
yganalyst <yg_analyst@naver.com>
2022-12-09 00:12:37 +0900

    20221208

--- programmers/DFSBFS_단어변환.py
+++ programmers/DFSBFS_단어변환.py
@@ -8,13 +8,20 @@
     https://school.programmers.co.kr/learn/courses/30/lessons/43163
     
 주의할점
-  - DFS로 잘 풀었으나, 문제조건(단어길이 3~10) 고려를 못함
-  - 그리고 바뀔수 있는 단어인지 확인할때, set으로 접근하면 안되고, 철자하나씩 확인해야함 (순서가 있어야 알지)
-  - 코드가 좀 길어졌는데, BFS로 푼 reference 참고하자
-  
-  
+  1. 내 답 (DFS)
+   - DFS로 잘 풀었으나, 문제조건(단어길이 3~10) 고려를 못함
+   - 그리고 바뀔수 있는 단어인지 확인할때, set으로 접근하면 안되고, 철자하나씩 확인해야함 (순서가 있어야 알지)
+   - 코드가 좀 길어졌는데, BFS로 푼 reference 참고하자
+  2. Reference (BFS)
+   - get_adjacent 함수에서 zip, yield 활용하여 현재 word에서 바뀔 수 있는 word들을 generator로 반환함
+   - dict.get(target,0) 은 target에 해당하는 count , 값이 없으면 0 을 반환
+  3. Reference (BFS)
+   - 블로그 참고
+   - 이것도 결국 최단경로 문제라서 BFS로 푸는 것이 좋음 !!!!! (계속 헷갈리네)
+   - BFS는 단계별로 가기때문에, 루프마다 count가 아니라 이전 word에서 몇번째 왔냐만 체크해주면 되는 것임
+   - 같은 깊이에서 하나씩 탐색하기때문에 결국 target에 도달하는 그 지점이 최단 경로 길이가 된다..!
 ** Reference
-
+https://muhly.tistory.com/86
 """
 
 
@@ -75,6 +82,7 @@ def get_adjacent(current, words):
 
         count = 0
         for c, w in zip(current, word):
+        # list(zip("hit",["hot", "dot", "dog", "lot", "log", "cog"]))
             if c != w:
                 count += 1
 
@@ -90,8 +98,49 @@ def solution(begin, target, words):
         current = queue.popleft()
 
         for next_word in get_adjacent(current, words):
+            print(dist)
             if next_word not in dist:
                 dist[next_word] = dist[current] + 1
                 queue.append(next_word)
 
     return dist.get(target, 0)
+
+begin = "hit"
+target = "cog"
+words = ["hot", "dot", "dog", "lot", "log", "cog"]
+solution(begin,target,words)
+
+# 3. Referecne (BFS 짧음)
+from collections import deque
+# queue를 위함
+
+def check(s, begin):
+    answer = 0
+    for i in range(len(s)):
+        if list(s)[i] != list(begin)[i]:
+            answer += 1
+    return True if answer == 1 else False
+
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+
+    queue = deque()
+    queue.append([begin, []])
+
+    while queue:
+        n, l = queue.popleft()
+        for word in words:
+            if word not in l and check(word, n):
+                if word == target:
+                    return len(l) + 1
+                temp = l[0:]
+                temp.append(word)
+                queue.append([word, temp])
+
+    return 0
+
+begin = "hit"
+target = "cog"
+words = ["hot", "dot", "dog", "lot", "log", "cog"]
