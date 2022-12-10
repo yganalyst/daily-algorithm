ad71db8a8ddb75657684297a659c55a2abb9d683
yganalyst <yg_analyst@naver.com>
2022-12-10 14:20:26 +0900

    20221210

---
+++ programmers/heap_더맵게.py
@@ -0,0 +1,17 @@
+"""
+20221210
+
+- 더 맵게
+- type: 힙(heap)
+- level: 2
+- Problem : 
+    https://school.programmers.co.kr/learn/courses/30/lessons/42626
+    
+주의할점
+
+*Referece
+
+"""
+
+# 1. 내 답
+
